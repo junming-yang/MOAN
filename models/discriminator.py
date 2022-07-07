fix dynamics gradient

models/discriminator.py
@@ -62,7 +62,7 @@ class Discriminator(nn.Module):
         for i in range(pre_mean.shape[0]):
             learner = torch.cat([model_input, pre_mean[i]], dim=1)
             real_loss = self._criterion(self.model(expert), torch.ones(batch_size, 1))
-            fake_loss = self._criterion(self.model(learner.detach()), torch.zeros(batch_size, 1))
+            fake_loss = self._criterion(self.model(learner), torch.zeros(batch_size, 1))
             g_loss = self._criterion(self.model(learner), torch.ones(batch_size, 1))
 
             # record expert and learner var
@@ -75,7 +75,7 @@ class Discriminator(nn.Module):
             generate_loss = g_loss.mean()
             loss_sum += discriminator_loss
             loss_gen_sum += generate_loss
-        self.logger.record("loss/d_loss", loss_sum.mean(), self.cnt)
+        self.logger.record("loss/d_loss", loss_sum.mean(), self.cnt, printed=False)
         self.logger.record("loss/g_loss", loss_gen_sum.mean(), self.cnt, printed=False)
         return loss_sum, loss_gen_sum
 

models/transition_model.py
@@ -100,9 +100,9 @@ class TransitionModel:
         train_mse_loss = torch.sum(train_mse_losses)
         train_var_loss = torch.sum(train_var_losses)
         train_d_loss, train_g_loss = self.discriminator.compute_loss(model_input, predictions)
-        d_coff = 0.001
+        d_coff = 0.1
+        # mse around 4, var around -32, d_loss around 8
         train_transition_loss = train_mse_loss + train_var_loss + d_coff * train_d_loss
-        # Todo: add discriminator
         train_transition_loss += 0.01 * torch.sum(self.model.max_logvar) - 0.01 * torch.sum(
             self.model.min_logvar)  # why
         if self.use_weight_decay:
@@ -114,9 +114,9 @@ class TransitionModel:
         # update transition model and discriminator
         self.model_optimizer.zero_grad()
         train_transition_loss.backward(retain_graph=True)
-        self.model_optimizer.step()
         if self.update_count > 0 and self.update_count % self.discriminator.get_interval == 0:
             self.discriminator.update(train_d_loss)
+        self.model_optimizer.step()
         self.update_count += 1
 
         # compute test loss for elite model